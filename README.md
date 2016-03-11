42afa3374bb79d954247bc8dfd1772db211b7c6f
Adriano T. Tanaka <adritanaka@gmail.com>
2016-03-10 23:28:58 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tdd-simple-project
---
+++ src/main/java/com/tdd/simple/project/TddSimpleProjectApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.simple.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSimpleProjectApplication.class, args);
+	}
+}
