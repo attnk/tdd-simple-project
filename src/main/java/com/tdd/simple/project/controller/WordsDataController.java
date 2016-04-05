1f066f1d04a8685f84cd1145c546588984f6f26b
Adriano T. Tanaka <adritanaka@gmail.com>
2016-04-04 21:10:44 -0300

    Inclusão de tratamento  de erro
    Inclusão do tratamento de erro no controller, forçando em caso de erro o retorno do HTTP Status 500.

--- src/main/java/com/tdd/simple/project/controller/WordsDataController.java
+++ src/main/java/com/tdd/simple/project/controller/WordsDataController.java
@@ -1,10 +1,14 @@
 package com.tdd.simple.project.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
+import javax.ws.rs.core.Response;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,9 +24,19 @@ public class WordsDataController {
 	 * Responsável por retornar os dados da palavra fornecida 
 	 * em formato JSON.
 	 * @return JSON com os dados da palavra informada.
+	 * @throws IOException 
 	 */
 	@RequestMapping(value = "/data/{word}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<WordsVo> getWordData(@PathParam(value = "{word}") String word) {
-		return new ArrayList<>();
+	public List<WordsVo> getWordData(@PathParam(value = "{word}") String word, HttpServletResponse response) {
+		try {
+			return new ArrayList<>();
+		} catch (Exception e) {
+			try {
+				response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			return null;
+		}
 	}
 }
